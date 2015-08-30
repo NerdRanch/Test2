cccbdc26652fcd57854baeb22866791f140dd459
NerdRanch <wenchaoding@foxmail.com>
2015-08-30 17:37:27 +0800

    Initial Commit

---
+++ Test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by chao on 15/8/30.
+//  Copyright (c) 2015年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test2
+//
+//  Created by chao on 15/8/30.
+//  Copyright (c) 2015年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
