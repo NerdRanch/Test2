fc94d2577aa85089d475ff1ebef18bdd30738347
NerdRanch <wenchaoding@foxmail.com>
2015-08-30 17:46:08 +0800

    test

---
+++ Test2/fafaf.h
@@ -0,0 +1,13 @@
+//
+//  fafaf.h
+//  Test2
+//
+//  Created by chao on 15/8/30.
+//  Copyright (c) 2015年 chao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface fafaf : NSObject
+
+@end
---
+++ Test2/fafaf.m
@@ -0,0 +1,13 @@
+//
+//  fafaf.m
+//  Test2
+//
+//  Created by chao on 15/8/30.
+//  Copyright (c) 2015年 chao. All rights reserved.
+//
+
+#import "fafaf.h"
+
+@implementation fafaf
+
+@end
